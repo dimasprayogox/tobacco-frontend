69697cdf53ff726e7fe19589c440cfee2c40f8b1
dimasprayogox <dimasprayogo9c@gmail.com>
2025-06-24 12:41:55 +0700

    style(hero section): update background

--- components/heroSection.vue
+++ components/heroSection.vue
@@ -1,6 +1,9 @@
 <template>
-    <NavbarSection />
-    <section class="relative bg-[#5a4a3a] flex flex-col-reverse md:flex-row mx-auto justify-between items-center gap-9 md:gap-4 max-w-[1300px] pt-24 overflow-hidden pb-9 px-4 md:px-8">
+  <NavbarSection />
+  
+  <section class="w-full bg-[#5a4a3a]">
+    
+    <div class="relative flex flex-col-reverse md:flex-row mx-auto justify-between items-center gap-9 md:gap-4 max-w-[1300px] pt-24 overflow-hidden pb-9 px-4 md:px-8">
       
       <div class="md:w-[520px] z-20">
         <h1 class="text-3xl md:text-[36px] lg:text-[46px] leading-tight lg:leading-[56px] text-white font-bold tracking-wide">
@@ -11,8 +14,10 @@
           Don't let leaf disease threaten your yield. Instantly upload a photo of a leaf, and our advanced AI will deliver a precise diagnosis and treatment plan in seconds.
         </p>
         <div class="flex gap-6 sm:gap-10">
-          <button class="uppercase font-bold text-xs rounded-[40px] py-2 lg:py-4 px-4 lg:px-9 text-[#5a4a3a] bg-gradient-to-r from-[#77B254] to-[#5B913B] transition-all duration-300 transform hover:scale-105 hover:to-[#77B254] hover:text-white hover:shadow-lg"> <a href="#identify">
-            Scan Your Leaf Now </a>
+          <button class="uppercase font-bold text-xs rounded-[40px] py-2 lg:py-4 px-4 lg:px-9 text-[#5a4a3a] bg-gradient-to-r from-[#77B254] to-[#5B913B] transition-all duration-300 transform hover:scale-105 hover:to-[#77B254] hover:text-white hover:shadow-lg">
+            <a href="#identify">
+              Scan Your Leaf Now
+            </a>
           </button>
         </div>
       </div>
@@ -21,5 +26,6 @@
         <img class="max-w-[453px] w-full" src="../public/assets/tobacco.png" alt="Tobacco Leaf">
       </div>
   
-    </section>
-  </template>
+    </div>
+  </section>
+</template>
